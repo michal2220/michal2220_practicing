e283d95a4d26d795cbb2cb44f496d36e5d561c08
Michal Gawlas <gawlas05@gmail.com>
2023-10-12 21:44:42 +0200

    Switch practice

---
+++ src/main/java/ForPractice/SwitchPractice.java
@@ -0,0 +1,22 @@
+package ForPractice;
+
+public class SwitchPractice {
+
+
+    public static void main(String[] args) {
+
+        String day = "Monday";
+
+        switch (day) {
+            case "Sunday":
+                System.out.println("It's Sunday!");
+                break;
+            case "Monday":
+                System.out.println("It's Monday!");
+                break;
+            case "Friday":
+                System.out.println("It's Friday!");
+                break;
+        }
+    }
+}
