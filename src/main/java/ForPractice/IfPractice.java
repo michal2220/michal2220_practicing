d56cb78b8ceaf7da23c3dddf28425c07297df4c0
Michal Gawlas <gawlas05@gmail.com>
2023-10-12 21:40:14 +0200

    First commit

---
+++ src/main/java/ForPractice/IfPractice.java
@@ -0,0 +1,17 @@
+package ForPractice;
+
+public class IfPractice {
+
+
+    public static void main(String[] args) {
+        int age = 75;
+
+        if (age == 18) {
+            System.out.println("True");
+        } else if (age >= 75) {
+            System.out.println("Other true");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
